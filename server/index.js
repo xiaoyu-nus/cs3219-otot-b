0f86236cc5860c76af6904ab4169f2eda7324fa9
xiaoyu-nus <tkgs.liu.xiaoyu@gmail.com>
2020-11-06 17:40:29 +0800

    Test

--- server/index.js
+++ server/index.js
@@ -55,11 +55,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-
-// Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
-
-
 // Launch app to listen to specified port
 app.listen(port, function () {
     console.log("Running RestHub on port " + port);
